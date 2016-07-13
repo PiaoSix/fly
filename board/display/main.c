a5ec758adcc47dee1512018d60dde79528da63e3
piaozhengyi <six_work@aliyun.com>
2016-07-13 23:17:38 +0800

    make sure draw lib        确定draw库

--- board/display/main.c
+++ board/display/main.c
@@ -4,29 +4,17 @@
 #include "display.h"
 
 
-static struct display_info d_info;
+
 int main(int argc, char **argv)
 {
-	d_info.fd = open(DEVNAME, O_RDWR);
+	struct six_draw *draw;
+	make_canvas(&draw);
 
-	if(d_info.fd < 0){
-		perror("open fb0");
-		goto err_open;
-	}
+	draw->print_display_info();
 
-	print_display_info(&d_info);
 
-	d_info.addr = mmap(NULL, d_info.fix.smem_len, PROT_WRITE, MAP_SHARED, d_info.fd, 0);
+	close_canvas();
 
-	draw_square(&d_info, 0, 0, 200, 100, RGB888(255, 255, 0));
-//  draw_line(&d_info, 10, 10, 100, 100, RGB888(255, 0, 0));
-
-	munmap(d_info.addr, d_info.fix.smem_len);
-	close(d_info.fd);
-
-	return 0;
-err_open:
-	return -1;
 }
 	
 
