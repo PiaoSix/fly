850c5c4f80adac6b03913143dd0da78f9a28a802
piaozhengyi <six_work@aliyun.com>
2016-07-14 00:18:50 +0800

    modify can't draw color bug

--- board/display/main.c
+++ board/display/main.c
@@ -8,9 +8,18 @@
 int main(int argc, char **argv)
 {
 	struct six_draw *draw;
-	make_canvas(&draw);
+	int ret = 0;
+	ret = make_canvas(&draw);
 
+	printf("%d", ret);
+	
 	draw->print_display_info();
+
+	for(int i = 0; i < 100 ; i++){
+		for(int j = 0; j < 200; j++){
+			draw->pixel(i, j, RGB888(255,0,0));
+		}
+	}
 
 
 	close_canvas();
